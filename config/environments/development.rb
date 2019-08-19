ae8b64a76d1c7738e284f7ba8c217bc38287ef7f
Mayuresh Srivastava <ICPL\mayuresh.srivastava@os-mayuresh.icpl.local>
2019-08-19 11:25:37 +0530

    Group views and mail configuration added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,16 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: GMAIL_USER_NAME,
+    password: GMAIL_PASSWORD,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
